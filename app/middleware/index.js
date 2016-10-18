e69d4057b33e062bd4bba0d4ae0d018da3fe635d
BMorse09 <benmorse09@gmail.com>
2016-10-18 09:26:12 -0400

    Initial back-end deployment

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const loader = require('lib/wiring/loader');
 
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:8080',
+  origin: process.env.CLIENT_ORIGIN || 'https://boyd-and-the-boys.github.io/nozama-front-end/',
 };
 
 const before = (app) => {
