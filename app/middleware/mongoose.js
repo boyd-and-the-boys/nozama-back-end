b379117b7b7dafa66a7f66475b9b16c6f22d1228
BMorse09 <benmorse09@gmail.com>
2016-10-18 13:35:18 -0400

    Back end deploy

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/nozama';
+
+let uri;
+if (process.env.NODE_ENV === 'production') {
+  uri = process.env.MONGODB_URI;
+} else {
+  uri = 'mongodb://localhost/boyd-and-the-boys';
+}
+
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
